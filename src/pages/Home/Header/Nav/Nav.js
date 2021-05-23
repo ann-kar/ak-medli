84db5321a31a3ccedc83d528caeab6c9b0714ce4
ann-kar <anna.paulina.karpiuk@gmail.com>
2021-05-23 17:27:43 +0200

    Nav: burger menu added

--- src/pages/Home/Header/Nav/Nav.js
+++ src/pages/Home/Header/Nav/Nav.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./Nav.scss";
+import NavList from "./NavList/NavList";
 
 function Nav() {
 
+    const [burger, setBurger] = useState(false);
+
+    const iconHandler = () => {
+        setBurger(!burger)
+    };
+
     return (
         <div className="Nav">
-            <div className="Nav__icon">
+            <div className="Nav__icon" onClick={iconHandler}>
                 <div className="Nav__icon__bar"/>
                 <div className="Nav__icon__bar"/>
                 <div className="Nav__icon__bar"/>
             </div>
+            {burger &&
+            <div className={`Nav__burger`}>
+                <NavList/>
+            </div>
+            }
         </div>
     )
 }
---
+++ src/pages/Home/Header/Nav/NavList/NavList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./NavList.scss";
+
+function NavList() {
+
+    return (
+            <ul className="NavList">
+                <li>Home</li>
+                <li>About</li>
+                <li>Courses</li>
+                <li>News</li>
+                <li>Contact</li>
+                <li>Log in</li>
+            </ul>
+    )
+}
+
+export default NavList;
