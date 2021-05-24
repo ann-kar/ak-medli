e5ea33c3fe685219bcf3e4046f3141634cbc6f13
ann-kar <anna.paulina.karpiuk@gmail.com>
2021-05-24 18:53:29 +0200

    Front, Stats: media adjustments

--- src/pages/Home/Front/Front.js
+++ src/pages/Home/Front/Front.js
@@ -11,9 +11,9 @@ function Front() {
                 <img className="Front__banner__image" src={hero} alt="woman reading & smiling"/>
             </div>
             <div className="Front__main">
-                <h1 className="Front__main__title">Check y
+                <h1 className="Front__main__title">Check <span className="nobr">y
                     <img className="Front__main__title__accent" src={ellipse} alt="ellipse"/>
-                    ur well-being</h1>
+                    ur</span> well-being</h1>
                 <h2 className="Front__main__description">We’re sure that deciding to check on your health is a big step,
                     even
                     if it doesn’t bother in any way. </h2>
